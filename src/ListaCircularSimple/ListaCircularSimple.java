0cc798c4157531e0c8b468bba6c15287f8cc243d
Juan Pablo Alvarez Petro <alvarezjp@uninorte.edu.co>
2024-10-21 09:16:39 -0500

    Exercise in class, correción ya que Danilo me hizo equivocarme

--- src/ListaCircularSimple/ListaCircularSimple.java
+++ src/ListaCircularSimple/ListaCircularSimple.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ListaCircularSimple;
 
 import ListaDoble.Auto;
 
-/**
- *
- * @author alvarezjp
- */
 public class ListaCircularSimple {
 
     private Nodo PTR;
--- src/ListaDoble/Main.java
+++ src/ListaDoble/Main.java
@@ -5,7 +5,7 @@ import ListaCircularSimple.ListaCircularSimple;
 public class Main {
 
     public static void main(String[] args) {
-        ListaDoble listaDoble = new ListaDoble();
+        ListaDoble listaDoble = new ListaDoble(); 
         listaDoble.registrarAuto("ABC-123", 2024, "Blanco", "Mercedes", 150);
         listaDoble.registrarAuto("ABC-124", 2025, "Gris", "Audi", 180);
         listaDoble.registrarAuto("ABC-125", 2001, "Rojo", "Jaguar", 300);
